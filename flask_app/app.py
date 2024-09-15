a9ec0485569724840375561f401d93b2e5299eac
Roger Menezes <rmenezes@netflix.com>
2024-09-15 07:51:22 -0700

    new folder with app files

--- flask_app/app.py
+++ flask_app/app.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def hello_world():
     return jsonify({"message": "Hello, World!"})
 
+# Define a REST GET endpoint
+@app.route('/api/h2', methods=['GET'])
+def hello_world_v2():
+    return jsonify({"message": "Hello, World2!"})
+
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)
